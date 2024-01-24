b1e9da413272e7c688c9ba5fb0a5026e11bfd917
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-01-23 21:24:35 -0300

    layout e vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-app theme="dark">
+        <v-app-bar :elevation="2">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+
+            <v-app-bar-title>
+                <NuxtLink to="/">Coffee Shop</NuxtLink>
+            </v-app-bar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <NuxtLink to="/cart"><v-icon>mdi-cart</v-icon></NuxtLink>
+
+            </v-btn>
+        </v-app-bar>
+        <!-- .... -->
+        <v-main>
+            <v-container class="d-flex flex-column">
+                <slot />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+  

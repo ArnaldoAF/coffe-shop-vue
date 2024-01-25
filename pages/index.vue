d4a3427ee4b3112bd7cd2929566ff199d92a57f4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-01-24 21:45:01 -0300

    mandar para o cart

--- pages/index.vue
+++ pages/index.vue
@@ -14,11 +14,14 @@
                 </v-container>
             </v-col>
             <v-col cols="4">
-                
+
                 <CartComponent />
-                <v-btn prepend-icon="mdi-send" size="x-large" class="w-100 mt-3 bg-green-darken-4">
-  Fazer Checkout
-</v-btn>
+                <NuxtLink to="/cart" :disabled="totalQtdItems == 0">
+                    <v-btn prepend-icon="mdi-send" size="x-large" class="w-100 mt-3 bg-green-darken-4" :disabled="totalQtdItems == 0">
+                        Fazer Checkout
+                    </v-btn>
+                </NuxtLink>
+
             </v-col>
         </v-row>
 
@@ -28,6 +31,8 @@
 <script setup>
 const coffeeListStore = useCoffeeStore()
 const { coffeeList } = storeToRefs(coffeeListStore)
+const cartCoffeeListStore = useCartStore()
+const { totalQtdItems } = storeToRefs(cartCoffeeListStore)
 
 onMounted(async () => {
     await coffeeListStore.initCoffeeList();
